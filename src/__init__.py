b2a22aeb560914f21004f4fc898fd012fc56fd4d
Jong Shin <jshin.m@gmail.com>
2021-12-09 16:41:20 -0500

    create local init

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from .dataset_generator import DatasetGenerator as DG
+from .dataset_loader import datasetLoader as DL
+from .model_analysis import modelAnalysis as MA
+from .train_model import trainModel as TM
+from .inductive_bias import IB
