a4cf1ec791f358b69bef1555d7186ed8acf096bf
Jong Shin <jshin.m@gmail.com>
2021-12-21 04:40:24 -0500

    keep dg up to date with current exp

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -202,54 +202,58 @@ class DatasetGenerator:
         # return p0-p1
         return p1/(p0+p1)
 
-    # @staticmethod
-    # def spiral_pdf(x, sig=0.25, rng=1, spirals=270):
+    @staticmethod
+    def spiral_pdf(x, sig=0.25, rng=1, spirals=270):
+        '''
+        method that draws gaussian posterior at each spiral center
+        '''
+        x0, x1 = DatasetGenerator.spiral_center(spirals, K=2, rng=rng)
+
+        mu01 = x0
+        mu02 = x1
+        cov = sig * np.eye(2)
+        inv_cov = np.linalg.inv(cov)
+
+        p0, p1 = 0, 0
+        for mu in mu01:
+            p0 += np.exp(-(x - mu)@inv_cov@(x-mu).T)
+        for mu in mu02:
+            p1 += np.exp(-(x - mu)@inv_cov@(x-mu).T)
+
+        p0 = p0/(2*np.pi*np.sqrt(np.linalg.det(cov)))
+        p1 = p1/(2*np.pi*np.sqrt(np.linalg.det(cov)))
+
+        return p0/(p0+p1)
+
+    # def spiral_pdf_jay(X, n_samples, n_class=2, noise=0.3):
     #     '''
-    #     method that draws gaussian posterior at each spiral center
+    #     Jayanta's correction
     #     '''
-    #     x0, x1 = DatasetGenerator.spiral_center(spirals, K=2, rng=rng)
+        
+    #     if n_class == 2:
+    #         turns = 2
+    #     elif n_class == 3:
+    #         turns = 2.5
+    #     elif n_class == 5:
+    #         turns = 3.5
+    #     elif n_class == 7:
+    #         turns = 4.5
+    #     else:
+    #         raise ValueError("sorry, can't currently surpport %s classes " % n_class)
 
-    #     mu01 = x0
-    #     mu02 = x1
-    #     cov = sig * np.eye(2)
-    #     inv_cov = np.linalg.inv(cov)
+    #     theta = np.linspace(
+    #             0, np.pi * 4 * turns / n_class, int(n_samples / n_class)
+    #         ) 
 
-    #     p0, p1 = 0, 0
-    #     for mu in mu01:
-    #         p0 += np.exp(-(x - mu)@inv_cov@(x-mu).T)
-    #     for mu in mu02:
-    #         p1 += np.exp(-(x - mu)@inv_cov@(x-mu).T)
+    #     likelihood = 0
+    #     x, y = X[:,0], X[:,1]
+    #     theta_ = np.arccos(x/np.sqrt(x**2 + y**2))
+    #     for t in theta:
+    #         likelihood += np.exp(
+    #             - (theta_ - t)**2/(2*noise)
+    #             )
 
-    #     p0 = p0/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-    #     p1 = p1/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-
-    #     return p0/(p0+p1)
-
-    def spiral_pdf(X, n_samples, n_class=2, noise=0.3):
-        if n_class == 2:
-            turns = 2
-        elif n_class == 3:
-            turns = 2.5
-        elif n_class == 5:
-            turns = 3.5
-        elif n_class == 7:
-            turns = 4.5
-        else:
-            raise ValueError("sorry, can't currently surpport %s classes " % n_class)
-
-        theta = np.linspace(
-                0, np.pi * 4 * turns / n_class, int(n_samples / n_class)
-            ) 
-
-        likelihood = 0
-        x, y = X[:,0], X[:,1]
-        theta_ = np.arccos(x/np.sqrt(x**2 + y**2))
-        for t in theta:
-            likelihood += np.exp(
-                - (theta_ - t)**2/(2*noise)
-                )
-
-        return likelihood/(np.sqrt(2*np.pi)*noise)
+    #     return likelihood/(np.sqrt(2*np.pi)*noise)
 
     @staticmethod
     def true_spiral(h=0.01, sig=0.00008, rng=3, cc=False, spirals=270, **kwarg):
@@ -340,79 +344,6 @@ class DatasetGenerator:
 
         return X[:, 0], X[:, 1], z
 
-        # if rotate:
-        #     mu01 = np.array([-0.5, 0])
-        #     mu02 = np.array([0.5, 0])
-        #     mu11 = np.array([0, 0.5])
-        #     mu12 = np.array([0, -0.5])
-        # else:
-        #     mu01 = np.array([-0.5, 0.5])
-        #     mu02 = np.array([0.5, -0.5])
-        #     mu11 = np.array([0.5, 0.5])
-        #     mu12 = np.array([-0.5, -0.5])
-        # cov = sig * np.eye(2)
-        # inv_cov = np.linalg.inv(cov)
-
-        # p0 = (
-        #     np.exp(-(x - mu01)@inv_cov@(x-mu01).T)
-        #     + np.exp(-(x - mu02)@inv_cov@(x-mu02).T)
-        # )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-
-        # p1 = (
-        #     np.exp(-(x - mu11)@inv_cov@(x-mu11).T)
-        #     + np.exp(-(x - mu12)@inv_cov@(x-mu12).T)
-        # )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-
-    @staticmethod
-    def pdf1(x, cov_scale=0.25):
-        mu01 = np.array([-0.5,0.5])
-        mu02 = np.array([0.5,-0.5])
-        mu11 = np.array([0.5,0.5])
-        mu12 = np.array([-0.5,-0.5])
-        cov = cov_scale* np.eye(2)
-        inv_cov = np.linalg.inv(cov) 
-
-        p01 = (
-            np.exp(-0.5*(x - mu01)@inv_cov@(x-mu01).T) 
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p02 = (
-            np.exp(-0.5*(x - mu02)@inv_cov@(x-mu02).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p11 = (
-            np.exp(-0.5*(x - mu11)@inv_cov@(x-mu11).T) 
-            # + np.exp(-0.5*(x - mu12)@inv_cov@(x-mu12).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p12 = (
-            np.exp(-0.5*(x - mu12)@inv_cov@(x-mu12).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-
-        return (p11+p12)/(p01+p02+p11+p12)
-
-    @staticmethod
-    def pdf4(x, cov_scale=0.25):
-        mu01 = np.array([-0.5,0.5])
-        mu02 = np.array([0.5,-0.5])
-        mu11 = np.array([0.5,0.5])
-        mu12 = np.array([-0.5,-0.5])
-        cov = cov_scale* np.eye(2)
-        inv_cov = np.linalg.inv(cov) 
-
-        p01 = (
-            np.exp(-0.5*(x - mu01)@inv_cov@(x-mu01).T) 
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p02 = (
-            np.exp(-0.5*(x - mu02)@inv_cov@(x-mu02).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p11 = (
-            np.exp(-0.5*(x - mu11)@inv_cov@(x-mu11).T) 
-            # + np.exp(-0.5*(x - mu12)@inv_cov@(x-mu12).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p12 = (
-            np.exp(-0.5*(x - mu12)@inv_cov@(x-mu12).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-
-        return np.max([p01,p02])/(p01+p02+p11+p12)
-
     @staticmethod
     def pdf(x, cov_scale=0.25):
         # added trace
@@ -431,33 +362,36 @@ class DatasetGenerator:
         # return np.max([p01,p02])/(p01+p02+p11+p12)
         return (p01+p02)/(p01+p02+p11+p12)
 
-    @staticmethod
-    def pdf4(x, cov_scale=0.25):
-        # added trace
-        mu01 = np.array([-0.5,0.5])
-        mu02 = np.array([0.5,-0.5])
-        mu11 = np.array([0.5,0.5])
-        mu12 = np.array([-0.5,-0.5])
-        cov = cov_scale* np.eye(2)
-        inv_cov = np.linalg.inv(cov) 
+    # @staticmethod
+    # def pdf(x, cov_scale=0.25):
+    # '''
+    # latest update by Jayanta
+    # '''
+    #     # added trace
+    #     mu01 = np.array([-0.5,0.5])
+    #     mu02 = np.array([0.5,-0.5])
+    #     mu11 = np.array([0.5,0.5])
+    #     mu12 = np.array([-0.5,-0.5])
+    #     cov = cov_scale* np.eye(2)
+    #     inv_cov = np.linalg.inv(cov) 
 
-        matrix_normal.pdf(x, mean=mu01, colcov=cov)
+    #     matrix_normal.pdf(x, mean=mu01, colcov=cov)
 
-        p01 = (
-            np.exp(-0.5* np.trace( (x - mu01)@inv_cov@(x-mu01).T) )
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p02 = (
-            np.exp(-0.5* np.trace( (x - mu02)@inv_cov@(x-mu02).T) )
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p11 = (
-            np.exp(-0.5* np.trace( (x - mu11)@inv_cov@(x-mu11).T) )
-            + np.exp(-0.5*(x - mu12)@inv_cov@(x-mu12).T)
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
-        p12 = (
-            np.exp(-0.5* np.trace( (x - mu12)@inv_cov@(x-mu12).T))
-        )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
+    #     p01 = (
+    #         np.exp(-0.5* np.trace( (x - mu01)@inv_cov@(x-mu01).T) )
+    #     )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
+    #     p02 = (
+    #         np.exp(-0.5* np.trace( (x - mu02)@inv_cov@(x-mu02).T) )
+    #     )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
+    #     p11 = (
+    #         np.exp(-0.5* np.trace( (x - mu11)@inv_cov@(x-mu11).T) )
+    #         + np.exp(-0.5*(x - mu12)@inv_cov@(x-mu12).T)
+    #     )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
+    #     p12 = (
+    #         np.exp(-0.5* np.trace( (x - mu12)@inv_cov@(x-mu12).T))
+    #     )/(2*np.pi*np.sqrt(np.linalg.det(cov)))
 
-        return np.max([p01,p02])/(p01+p02+p11+p12)
+    #     return np.max([p01,p02])/(p01+p02+p11+p12)
 
     @staticmethod
     def true_Uxor(rng=3, h=0.01, cc=False, **kwarg):
